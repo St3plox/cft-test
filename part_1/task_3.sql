3bc522a2641e89fc7585374a3e7608dc5bb8580a
Egor Stepanenko <76905680+St3plox@users.noreply.github.com>
2025-07-08 15:24:19 +0700

    Update task_3.sql

--- part_1/task_3.sql
+++ part_1/task_3.sql
@@ -1,48 +1,53 @@
-CREATE OR REPLACE PROCEDURE redem_debt(
-  pTYPE VARCHAR2,
-  pSUMM NUMBER
-)
-IS
-  CURSOR c IS
-    SELECT ROWID rid, date_b, s_in, s_debt, s_redeem
-      FROM DEBTS
-     WHERE TYPE = pTYPE
-     ORDER BY DATE_B;
-  
-  v_summ_left NUMBER := pSUMM;
-BEGIN
-  FOR rec IN c LOOP
-    IF v_summ_left <= 0 THEN
-      EXIT;
-    END IF;
-    
-    -- сумма текущего долга
-    v_current_debt := rec.s_in + rec.s_debt + rec.s_redeem;
-    
-    -- доля оплаты (сумма, которую мы можем покрыть)
-    v_to_pay := LEAST(v_summ_left, v_current_debt);
-    
-    -- сколько останется долга
-    v_new_s_in := GREATEST(0, rec.s_in - v_to_pay);
-    
-    -- пересчитаем, сколько ещё осталось после оплаты
-    v_summ_left := v_summ_left - (rec.s_in - v_new_s_in);
-    
-    -- если оплатили больше, остаток идёт на текущий долг
-    IF v_summ_left > 0 THEN
-      v_new_s_redeem := GREATEST(0, rec.s_redeem - v_summ_left);
-      v_summ_left := v_summ_left - (rec.s_redeem - v_new_s_redeem);
-    ELSE
-      v_new_s_redeem := rec.s_redeem;
-    END IF;
+CREATE OR REPLACE PROCEDURE redem_debt(pTYPE VARCHAR2, pSUMM NUMBER) IS
+  CURSOR cur_debts IS
+    SELECT rowid AS rid, DATE_B, S_IN, S_DEBT, S_REDEM
+    FROM DEBTS
+    WHERE TYPE = pTYPE
+    ORDER BY DATE_B;
 
-    UPDATE DEBTS
-       SET S_IN = v_new_s_in,
-           S_REDEM = v_new_s_redeem
-     WHERE ROWID = rec.rid;
-    
+  vSUMM NUMBER := pSUMM;
+  cumulative_debt NUMBER := 0;
+  total_paid NUMBER := 0;
+BEGIN
+  FOR rec IN cur_debts LOOP
+    DECLARE
+      v_current_debt NUMBER;
+      v_pay_now NUMBER;
+      v_new_S_IN NUMBER;
+      v_new_S_REDEM NUMBER;
+    BEGIN
+      IF rec.rid = 1 THEN
+        cumulative_debt := rec.S_IN + rec.S_DEBT - rec.S_REDEM;
+      ELSE
+        cumulative_debt := cumulative_debt + rec.S_DEBT - rec.S_REDEM;
+      END IF;
+      
+      IF vSUMM >= (cumulative_debt - total_paid) THEN
+        v_pay_now := cumulative_debt - total_paid;
+      ELSE
+        v_pay_now := vSUMM;
+      END IF;
+
+      v_new_S_REDEM := rec.S_REDEM + v_pay_now;
+
+      IF v_pay_now = (cumulative_debt - total_paid) THEN
+        v_new_S_IN := 0;
+      ELSE
+        v_new_S_IN := cumulative_debt - total_paid - v_pay_now;
+      END IF;
+
+      UPDATE DEBTS
+      SET S_IN = v_new_S_IN,
+          S_REDEM = v_new_S_REDEM
+      WHERE rowid = rec.rid;
+
+      total_paid := total_paid + v_pay_now;
+      vSUMM := vSUMM - v_pay_now;
+
+      IF vSUMM <= 0 THEN
+        EXIT;
+      END IF;
+    END;
   END LOOP;
-  
   COMMIT;
 END;
-/
